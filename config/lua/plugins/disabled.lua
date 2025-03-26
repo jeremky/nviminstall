cb52213660a3f0ff099215afd0a1c2f951b4969d
JeremKy <jeremky@me.com>
2025-03-26 15:43:52 +0100

    Mise à jour

--- config/lua/plugins/disabled.lua
+++ config/lua/plugins/disabled.lua
@@ -1,5 +1,6 @@
 return {
 	{ "windwp/nvim-ts-autotag", enabled = false },
+	{ "Saghen/blink.cmp", enabled = false },
 	{ "akinsho/bufferline.nvim", enabled = false },
 	{ "folke/flash.nvim", enabled = false },
 	{ "MagicDuck/grug-far.nvim", enabled = false },
@@ -8,5 +9,4 @@ return {
 	{ "folke/tokyonight.nvim", enabled = false },
 	{ "folke/trouble.nvim", enabled = false },
 	{ "folke/ts-comments.nvim", enabled = false },
-	{ "Saghen/blink.cmp", enabled = false },
 }
